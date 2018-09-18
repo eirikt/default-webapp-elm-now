9da7cc3c5eb0139b0599af6e857b2466fded5590
Eirik Torske <eirik.torske@gmail.com>
2018-09-18 20:45:16 +0200

    v0.3.1: webpack development mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
+    mode: 'development',
     entry: path.join(__dirname, 'src/index.js'),
     output: {
         path: path.join(__dirname, 'build'),
