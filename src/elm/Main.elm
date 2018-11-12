dc326f3a93bdfb893c1b9bb148a6bdc86ffe3670
Eirik Torske <eirik.torske@gmail.com>
2018-11-12 12:50:16 +0100

    v0.7.4: Elm: Browser.sandbox

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,2 +1,14 @@
+module Main exposing (main)
+
+import Browser
 import Html
-main = Html.text "Default Webapp | Elm & ZEIT Now edition [v0.7.3]"
+
+
+main =
+    Browser.sandbox
+        { init = "Default Webapp | Elm & ZEIT Now edition [v0.7.4]"
+        , view = \model -> Html.text model
+
+        -- Never invoked ('init' is the only "update", no user events yet)
+        , update = \msg -> \model -> "N/A"
+        }
